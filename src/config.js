ce91d5dee0c4451ae7bf99ea4c4a147dd461f9ed
Michael Yip <michael@poyu.co.uk>
2021-10-22 18:49:40 +0100

    Refactoring use scheduler const

--- src/config.js
+++ src/config.js
@@ -13,6 +13,7 @@ var WISH_TALLY_DASHBOARD_SHEET_NAME = "Dashboard";
 var WISH_TALLY_CHANGELOG_SHEET_NAME = "Changelog";
 var WISH_TALLY_PITY_CHECKER_SHEET_NAME = "Pity Checker";
 var WISH_TALLY_EVENTS_SHEET_NAME = "Events";
+var WISH_TALLY_CHARACTERS_OLD_SHEET_NAME = "Constellation";
 var WISH_TALLY_CHARACTERS_SHEET_NAME = "Characters";
 var WISH_TALLY_WEAPONS_SHEET_NAME = "Weapons";
 var WISH_TALLY_RESULTS_SHEET_NAME = "Results";
@@ -22,3 +23,75 @@ var WISH_TALLY_NAME_OF_WISH_HISTORY = [WISH_TALLY_CHARACTER_EVENT_WISH_SHEET_NAM
 // AutoHotkey Const
 var AUTOHOTKEY_SHEET_NAME = "AutoHotkey";
 var AUTOHOTKEY_SCRIPT_SHEET_NAME = "AutoHotkey-Script";
+
+// Import Const
+var IMPORT_STATUS_COMPLETE = "COMPLETE";
+var IMPORT_STATUS_FAILED = "FAILED";
+var IMPORT_STATUS_WISH_HISTORY_COMPLETE = "DONE";
+var IMPORT_STATUS_WISH_HISTORY_NOT_FOUND = "NOT FOUND";
+var IMPORT_STATUS_WISH_HISTORY_EMPTY = "EMPTY";
+
+// Scheduler Const
+var SCHEDULER_TRIGGER_ON_TEXT = "ON";
+var SCHEDULER_TRIGGER_OFF_TEXT = "OFF";
+var SCHEDULER_RUN_AT_WHICH_DAY = {
+    "Monday": ScriptApp.WeekDay.MONDAY,
+    "Tuesday": ScriptApp.WeekDay.TUESDAY,
+    "Wednesday": ScriptApp.WeekDay.WEDNESDAY,
+    "Thursday": ScriptApp.WeekDay.THURSDAY,
+    "Friday": ScriptApp.WeekDay.FRIDAY,
+    "Saturday": ScriptApp.WeekDay.SATURDAY,
+    "Sunday": ScriptApp.WeekDay.SUNDAY
+  };
+  var SCHEDULER_RUN_AT_HOUR = {
+    "Run at 1:00": 1,
+    "Run at 2:00": 2,
+    "Run at 3:00": 3,
+    "Run at 4:00": 4,
+    "Run at 5:00": 5,
+    "Run at 6:00": 6,
+    "Run at 7:00": 7,
+    "Run at 8:00": 8,
+    "Run at 9:00": 9,
+    "Run at 10:00": 10,
+    "Run at 11:00": 11,
+    "Run at 12:00": 12,
+    "Run at 13:00": 13,
+    "Run at 14:00": 14,
+    "Run at 15:00": 15,
+    "Run at 16:00": 16,
+    "Run at 17:00": 17,
+    "Run at 18:00": 18,
+    "Run at 19:00": 19,
+    "Run at 20:00": 20,
+    "Run at 21:00": 21,
+    "Run at 22:00": 22,
+    "Run at 23:00": 23,
+    "Run at Midnight": 0
+  };
+  var SCHEDULER_RUN_AT_EVERY_HOUR = {
+    "Every hour": 1,
+    "Every 2 hours": 2,
+    "Every 3 hours": 3,
+    "Every 4 hours": 4,
+    "Every 5 hours": 5,
+    "Every 6 hours": 6,
+    "Every 7 hours": 7,
+    "Every 8 hours": 8,
+    "Every 9 hours": 9,
+    "Every 10 hours": 10,
+    "Every 11 hours": 11,
+    "Every 12 hours": 12,
+    "Every 13 hours": 13,
+    "Every 14 hours": 14,
+    "Every 15 hours": 15,
+    "Every 16 hours": 16,
+    "Every 17 hours": 17,
+    "Every 18 hours": 18,
+    "Every 19 hours": 19,
+    "Every 20 hours": 20,
+    "Every 21 hours": 21,
+    "Every 22 hours": 22,
+    "Every 23 hours": 23,
+    "Every 24 hours": 24
+  };
--- src/scheduler.js
+++ src/scheduler.js
@@ -1,70 +1,3 @@
-var triggerOnString = "ON";
-var triggerOffString = "OFF";
-
-var runAtWhichDay = {
-  "Monday": ScriptApp.WeekDay.MONDAY,
-  "Tuesday": ScriptApp.WeekDay.TUESDAY,
-  "Wednesday": ScriptApp.WeekDay.WEDNESDAY,
-  "Thursday": ScriptApp.WeekDay.THURSDAY,
-  "Friday": ScriptApp.WeekDay.FRIDAY,
-  "Saturday": ScriptApp.WeekDay.SATURDAY,
-  "Sunday": ScriptApp.WeekDay.SUNDAY
-};
-
-var runAtHour = {
-  "Run at 1:00": 1,
-  "Run at 2:00": 2,
-  "Run at 3:00": 3,
-  "Run at 4:00": 4,
-  "Run at 5:00": 5,
-  "Run at 6:00": 6,
-  "Run at 7:00": 7,
-  "Run at 8:00": 8,
-  "Run at 9:00": 9,
-  "Run at 10:00": 10,
-  "Run at 11:00": 11,
-  "Run at 12:00": 12,
-  "Run at 13:00": 13,
-  "Run at 14:00": 14,
-  "Run at 15:00": 15,
-  "Run at 16:00": 16,
-  "Run at 17:00": 17,
-  "Run at 18:00": 18,
-  "Run at 19:00": 19,
-  "Run at 20:00": 20,
-  "Run at 21:00": 21,
-  "Run at 22:00": 22,
-  "Run at 23:00": 23,
-  "Run at Midnight": 0
-};
-
-var runAtEveryHour = {
-  "Every hour": 1,
-  "Every 2 hours": 2,
-  "Every 3 hours": 3,
-  "Every 4 hours": 4,
-  "Every 5 hours": 5,
-  "Every 6 hours": 6,
-  "Every 7 hours": 7,
-  "Every 8 hours": 8,
-  "Every 9 hours": 9,
-  "Every 10 hours": 10,
-  "Every 11 hours": 11,
-  "Every 12 hours": 12,
-  "Every 13 hours": 13,
-  "Every 14 hours": 14,
-  "Every 15 hours": 15,
-  "Every 16 hours": 16,
-  "Every 17 hours": 17,
-  "Every 18 hours": 18,
-  "Every 19 hours": 19,
-  "Every 20 hours": 20,
-  "Every 21 hours": 21,
-  "Every 22 hours": 22,
-  "Every 23 hours": 23,
-  "Every 24 hours": 24
-};
-
 function createWeeklyTrigger(functionName, day, hour, minutes) {
   ScriptApp.newTrigger(functionName)
   .timeBased()
@@ -81,7 +14,7 @@ function createHourlyTrigger(functionName, hour, minutes) {
 }
 
 function setTriggerDataManagement() {
-  var settingsSheet = SpreadsheetApp.getActive().getSheetByName('Settings');
+  var settingsSheet = SpreadsheetApp.getActive().getSheetByName(WISH_TALLY_SETTINGS_SHEET_NAME);
   removeTriggers();
   if (settingsSheet) {
     var isScheduleAvailable = false;
@@ -89,44 +22,44 @@ function setTriggerDataManagement() {
     // Update Items List trigger
     var updateItemsSchedule = settingsSheet.getRange("E20").getValue();
     
-    var gotHour = runAtHour[updateItemsSchedule];
+    var gotHour = SCHEDULER_RUN_AT_HOUR[updateItemsSchedule];
     if (gotHour || gotHour == 0) {
       isScheduleAvailable = true;
-      createWeeklyTrigger("updateItemsListTriggered", runAtWhichDay["Sunday"], gotHour, 1);
+      createWeeklyTrigger("updateItemsListTriggered", SCHEDULER_RUN_AT_WHICH_DAY["Sunday"], gotHour, 1);
       settingsSheet.getRange("E21").setValue(updateItemsSchedule);
     } else {
-      settingsSheet.getRange("E21").setValue(triggerOffString);
+      settingsSheet.getRange("E21").setValue(SCHEDULER_TRIGGER_OFF_TEXT);
     }
 
     // Sort trigger
     var sortRangeSchedule = settingsSheet.getRange("E27").getValue();
-    var gotRunHourly = runAtEveryHour[sortRangeSchedule];
+    var gotRunHourly = SCHEDULER_RUN_AT_EVERY_HOUR[sortRangeSchedule];
     if (gotRunHourly) {
       isScheduleAvailable = true;
       createHourlyTrigger("sortRangesTriggered", gotRunHourly, 0);
       settingsSheet.getRange("E28").setValue(sortRangeSchedule);
     } else {
-      settingsSheet.getRange("E28").setValue(triggerOffString);
+      settingsSheet.getRange("E28").setValue(SCHEDULER_TRIGGER_OFF_TEXT);
     }
 
     // scheduledTrigger(1,00, "updateItemsListTriggered");
     if (isScheduleAvailable) {
-      settingsSheet.getRange("E16").setValue(triggerOnString);
+      settingsSheet.getRange("E16").setValue(SCHEDULER_TRIGGER_ON_TEXT);
     } else {
-      settingsSheet.getRange("E16").setValue(triggerOffString);
+      settingsSheet.getRange("E16").setValue(SCHEDULER_TRIGGER_OFF_TEXT);
     }
   }
 }
 
 function updateItemsListTriggered() {
-  var settingsSheet = SpreadsheetApp.getActive().getSheetByName('Settings');
+  var settingsSheet = SpreadsheetApp.getActive().getSheetByName(WISH_TALLY_SETTINGS_SHEET_NAME);
   settingsSheet.getRange("E22").setValue(new Date());
   updateItemsList();
   settingsSheet.getRange("E23").setValue(new Date());
 }
 
 function sortRangesTriggered() {
-  var settingsSheet = SpreadsheetApp.getActive().getSheetByName('Settings');
+  var settingsSheet = SpreadsheetApp.getActive().getSheetByName(WISH_TALLY_SETTINGS_SHEET_NAME);
   settingsSheet.getRange("E29").setValue(new Date());
   sortCharacterEventWishHistory();
   sortPermanentWishHistory();
@@ -161,12 +94,12 @@ function removeTriggers() {
 }
 
 function removeTriggerDataManagement() {
-  var settingsSheet = SpreadsheetApp.getActive().getSheetByName('Settings');
+  var settingsSheet = SpreadsheetApp.getActive().getSheetByName(WISH_TALLY_SETTINGS_SHEET_NAME);
   removeTriggers();
   var title = "Remove Schedule";
   var message = "All schedule has been removed";
   SpreadsheetApp.getActiveSpreadsheet().toast(message, title);
-  settingsSheet.getRange("E16").setValue(triggerOffString);
-  settingsSheet.getRange("E21").setValue(triggerOffString);
-  settingsSheet.getRange("E28").setValue(triggerOffString);
+  settingsSheet.getRange("E16").setValue(SCHEDULER_TRIGGER_OFF_TEXT);
+  settingsSheet.getRange("E21").setValue(SCHEDULER_TRIGGER_OFF_TEXT);
+  settingsSheet.getRange("E28").setValue(SCHEDULER_TRIGGER_OFF_TEXT);
 }
