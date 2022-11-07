a83a26500f64c85c289b82cf42f07e60ff50d808
Michael Yip <michael@poyu.co.uk>
2022-11-07 20:56:01 +0000

    Update dropdown with selection of tick box.

---
+++ src/edit.js
@@ -0,0 +1,15 @@
+function onEdit(e) {
+    const sheet = e.range.getSheet(); 
+    if(sheet.getName() == WISH_TALLY_CHARACTERS_SHEET_NAME || sheet.getName() == WISH_TALLY_WEAPONS_SHEET_NAME) {
+        if (e.value == "TRUE") {
+            var allowableColumns = sheet.getRange(1,12).getValue();
+            allowableColumns = String(allowableColumns).split(",");
+            if (allowableColumns.includes(String(e.range.columnStart))) {
+                sheet.getRange(e.range.rowStart, e.range.columnStart).setValue(false);
+                var characterName = sheet.getRange(e.range.rowStart, e.range.columnStart+1).getValue();
+                var indexRow = sheet.getRange(1,e.range.columnStart).getValue();
+                sheet.getRange(indexRow, e.range.columnStart).setValue(characterName);
+            }
+        }
+    }
+}
